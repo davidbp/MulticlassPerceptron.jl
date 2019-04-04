9512badb9150e82cd23ed6ee30c0cc08d43aca29
David Buchaca Prats <davidbuchaca@gmail.com>
2019-04-04 18:18:49 +0200

    Update basic_usage_train.jl

--- basic_usage_train.jl
+++ basic_usage_train.jl
@@ -1,10 +1,5 @@
 
-using MLDatasets
-
-if "/Users/david.buchaca/Documents/git_stuff/MulticlassPerceptron.jl/src" ∉  LOAD_PATH
-     push!(LOAD_PATH,"/Users/david.buchaca/Documents/git_stuff/MulticlassPerceptron.jl/src")
-     print("Library added")
-end
+using MLDatasets, Statistics
 using MulticlassPerceptron
 
 ## Prepare data
@@ -17,7 +12,6 @@ test_y  = test_y .+ 1;
 train_x = reshape(train_x, 784, 60000);
 test_x  = reshape(test_x,  784, 10000);
 
-
 ## Define model and train it
 scores = []
 n_features = size(train_x, 1);
@@ -25,4 +19,11 @@ n_classes =  length(unique(train_y));
 perceptron = MulticlassPerceptronClassifier(Float32, n_classes, n_features);
 println("\nStart Learning\n")
 MulticlassPerceptron.fit!(perceptron, train_x, train_y, scores;  print_flag=true, n_epochs=100);
+y_hat_test = MulticlassPerceptron.predict(perceptron, test_x);
+y_hat_train = MulticlassPerceptron.predict(perceptron,train_x)
 println("\nLearning Finished\n")
+
+println("Results:")
+println("Train accuracy:", mean(y_hat_train .==train_y))
+println("Test accuracy:", mean(y_hat_test .==test_y))
+
